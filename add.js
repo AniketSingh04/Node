5c7e1752441fe13b91689464a21160a5061c02f5
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-04-20 16:28:09 +0530

    Initial files

---
+++ add.js
@@ -0,0 +1,12 @@
+//1
+// const add = (a,b) => {
+//     return a+b;
+// };
+//console.log(`Sum is - ${add(3,4)}`);
+//module.exports = add; //exporting add function
+
+//2
+const a ="aniket";
+const b ="raj";
+const c ="singh";
+module.exports = {a,b,c}; //exporting multiple variables in object
---
+++ exports.js
@@ -0,0 +1,13 @@
+exports.sayHello = () => {
+    console.log("Hello");
+};
+
+module.exports.sayThanks = function() {
+    console.log("Thanks");
+};
+
+module.exports.sayBye = function() {
+    console.log("Bye");
+};
+
+console.log(module);
